762454854a714fa82bfeec245b88666851be691d
ekalashnikov <e.kalashnikov@tinkoff.ru>
2021-10-27 23:39:25 +0700

    cells

--- src/scripts/components/table/table.component.ts
+++ src/scripts/components/table/table.component.ts
@@ -27,28 +27,35 @@ const createTable = (rowsCount = 15): string => {
     const colsCount = CODES.Z - CODES.A + 1;
     const rows: string[] = [];
 
-    const cells = new Array(colsCount)
+    const cellsHeader = new Array(colsCount)
         .fill('')
         .map(toChar)
         .map(toColumn)
         .join('');
 
-    rows.push(createRow(cells));
+    rows.push(createRow(cellsHeader, null));
 
     for (let i = 0; i < rowsCount; i++) {
-        rows.push(createRow());
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('');
+
+        rows.push(createRow(cells, i + 1));
     }
 
     return rows.join('');
 };
 
-const createRow = (content: string = '') => {
+const createRow = (content: string = '', index: number) => {
     return `
         <div class="row">
-            <div class="row-info"></div>
-            <div class="row-data row-header">
-                ${content}
-            </div>
+            <div class="row-info">${index ?? ''}</div>
+            <div class="row-data ${index === null ? 'row-header' : ''}">${content}</div>
         </div>
     `;
+};
+
+const toCell = () => {
+    return `<div class="cell" contenteditable="true"></div>`;
 };
